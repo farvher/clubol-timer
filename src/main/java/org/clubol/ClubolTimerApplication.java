7ce365617b1ad4cd98e6c5a3d91d9261626a2735
farvher <fesanmiguel@misena.edu.co>
2017-03-15 13:35:44 -0500

    ADD first commit

---
+++ src/main/java/org/clubol/ClubolTimerApplication.java
@@ -0,0 +1,12 @@
+package org.clubol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubolTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubolTimerApplication.class, args);
+	}
+}
